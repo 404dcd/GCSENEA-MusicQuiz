988bfbc4a4d0ba380e69e3a9cd425112e4550157
404dcd <ben.github@thegenius.org.uk>
2021-04-04 18:32:10 +0100

    Recover from git errors, and add more work to make game almost fully functional

--- quiz/game.py
+++ quiz/game.py
@@ -1,5 +1,5 @@
 from flask import (
-    Blueprint, render_template, request, g
+    Blueprint, render_template, request, g, flash, url_for, redirect
 )
 
 from quiz.db import get_db
@@ -8,16 +8,21 @@ bp = Blueprint("game", __name__, url_prefix="/")
 
 
 def new_song(db, uid, purge_used=False):
-    used = db.execute(
-        "SELECT used FROM game WHERE userid = ?", (uid,)
-    ).fetchone()["used"]
+    used = ""
+    if not purge_used:
+        used = db.execute(
+            "SELECT used FROM game WHERE userid = ?", (uid,)
+        ).fetchone()["used"]
 
-    song = db.execute(
+    songrecord = db.execute(
         f"SELECT id FROM songs WHERE id NOT IN ({used}) ORDER BY RANDOM() LIMIT 1"
-    ).fetchone()["id"]
+    ).fetchone()
+    if songrecord is None:
+        return (new_song(db, uid, purge_used=True)[0], True)
+    song = songrecord["id"]
 
     db.execute(
-        "UPDATE game SET currsong = ? WHERE userid = ?", (song, uid)
+        "UPDATE game SET currsong = ?, attempts = 0 WHERE userid = ?", (song, uid)
     )
     db.commit()
 
@@ -30,34 +35,89 @@ def new_song(db, uid, purge_used=False):
             "UPDATE game SET used = ? WHERE userid = ?", (f"{used},{song}", uid)
         )
     db.commit()
-    return song
+    return (song, False)
 
 
 @bp.route("/play", methods=("GET", "POST"))
 def play():
     db = get_db()
-
-    if request.method == "POST":
-        new_song(db, g.user["userid"], purge_used=False)
-
-    songid = db.execute(
-        "SELECT currsong FROM game WHERE userid = ?", (g.user["userid"],)
+    uid = g.user["userid"]
+    songrecord = db.execute(
+        "SELECT currsong FROM game WHERE userid = ?", (uid,)
     ).fetchone()
-    if songid is None:  # Add user to game database on first go
+    songid = 0
+
+    if songrecord is None:  # Add user to game database on first go
         db.execute(
             "INSERT INTO game (userid, display, currscore, currsong, attempts, used, highscore) VALUES (?, ?, ?, ?, ?, ?, ?)",
-            (g.user["userid"], "", 0, 0, 0, "", 0)
+            (uid, "", 0, 0, 0, "", 0)
         )
         db.commit()
-        songid = new_song(db, g.user["userid"], purge_used=True)
+        songid = new_song(db, uid, purge_used=True)[0]
+
     else:
-        songid = songid["currsong"]
+        songid = songrecord["currsong"]
+        song = db.execute(
+            "SELECT title FROM songs WHERE id = ?", (songid,)
+        ).fetchone()
+
+        if request.method == "POST":
+            words = song["title"].split()
+            count = 0
+            for wid, word in request.form.items():
+                correct = words[int(wid[1:])][1:]
+                if word.lower().strip() == correct:
+                    count += 1
+
+            attempts = db.execute(
+                "SELECT attempts FROM game WHERE userid = ?", (uid,)
+            ).fetchone()["attempts"]
+            if count == len(request.form):  # if song was correct
+                pts = 1
+                if attempts == 0:
+                    pts = 3
+                db.execute(
+                    "UPDATE game SET currscore = currscore + ? WHERE userid = ?", (pts, uid)
+                )
+                db.commit()
+                songid, ranout = new_song(db, uid)
+
+                if ranout:
+                    flash("You just correctly guessed all songs in the database!")
+                    return redirect(url_for("index"))
+                else:
+                    flash("Good job, that's correct.")
+
+            elif attempts == 0:  # have another go
+                db.execute(
+                    "UPDATE game SET attempts = attempts + 1 WHERE userid = ?", (uid,)
+                )
+                db.commit()
+                flash(f"You entered {count} of {len(request.form)} correctly. Have another go.")
+
+            else:  # game over
+                flash(f"The correct title was '{song['title']}'")
+                score = db.execute(
+                    "SELECT currscore, highscore FROM game WHERE userid = ?", (uid,)
+                ).fetchone()
+                msg = f"You got {score['currscore']}"
+
+                if score["currscore"] > score["highscore"]:
+                    flash(msg + " - your new personal best!")
+                    db.execute(
+                        "UPDATE game SET highscore = currscore WHERE userid = ?", (uid,)
+                    )
+                    db.commit()
+                else:
+                    flash(msg + ".")
+
+                db.execute(
+                    "UPDATE game SET currscore = 0 WHERE userid = ?", (uid,)
+                )
+                db.commit()
+                songid = new_song(db, uid, purge_used=True)[0]
 
     song = db.execute(
         "SELECT * FROM songs WHERE id = ?", (songid,)
     ).fetchone()
-    words = []
-    c = 0
-    for w in song["title"].split():
-        words.append({"id": c, "letter": w[0]})
-    return render_template("game.html", words=words, artist=song["artist"])
+    return render_template("game.html", words=song["title"].split(), artist=song["artist"])
